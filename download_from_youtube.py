42769fd7d772fea4e73a931c126d280e0927ed47
Anton Krasnikau <anton.krasnikov1@gmail.com>
2018-07-23 12:55:28 +0000

    add more functionality in choose

--- download_from_youtube.py
+++ download_from_youtube.py
@@ -3,16 +3,22 @@ import sys
 import os
 from pytube import YouTube
 youtube_url = input('Input YouTube url: ')
+#youtube_url = 'https://youtu.be/Cav9lT-UmW0'
 yt = YouTube(youtube_url)
 path = sys.path[0] + '/'
 title = yt.title.replace('/','').replace('.','').replace(':','')
-choose_va = input('Download video or audio (write v or a): ')
-if choose_va == 'v':
-    print('Start downloading video: ')
-    yt.streams.first().download()
-elif choose_va == 'a':
-    print('Start downloading audio: ')
-    yt.streams.filter(type='audio').filter(subtype='mp4').first().download()
-    os.rename(path + title + '.mp4', path + title + '.mp3')
-else:
-    print('Wrong input')
+choose_va = ''
+while(choose_va.lower() not in ['v','video','a','audio']):
+    choose_va = input('Download video or audio (write v or a). Write quit or q to exit: ')
+    if choose_va.lower() == 'v' or choose_va.lower() == 'video':
+        print('Start downloading video: ')
+        #yt.streams.filter(type='video').filter(subtype='mp4').get_by_itag(22).download()
+        yt.streams.first().download()
+    elif choose_va.lower() == 'a' or choose_va.lower() == 'audio':
+        print('Start downloading audio: ')
+        yt.streams.filter(type='audio').filter(subtype='mp4').first().download()
+        os.rename(path + title + '.mp4', path + title + '.mp3')
+    elif choose_va.lower() == 'q' or choose_va.lower() == 'quit':
+        sys.exit()
+    else:
+        print('Wrong input')
